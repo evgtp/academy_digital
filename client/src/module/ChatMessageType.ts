58785c641bbe9853a5d9b0905c2f8293e0312557
Evgeny Tupitsyn <e_v_t_95@mail.ru>
2024-09-30 14:16:19 +0400

    debag project

--- client/src/module/ChatMessageType.ts
+++ client/src/module/ChatMessageType.ts
@@ -1,28 +1,6 @@
-export interface User {
-  username: string;
-  password: string;
-}
-
-// export interface TypeMessage {
-//   type: string;
-//   sender?: string;
-//   message?: string;
-//   success?: boolean;
-//   error?: string;
-// }
-
 export interface TypeMessage {
   type?: string;
   sender?: string;
-  message?: string;
-  success?: boolean;
-  error?: string;
-}
-
-export interface WebSocketMessage {
-  type: "auth" | "message" | "register";
-  username?: string;
-  password?: string;
   message?: string;
   success?: boolean;
   error?: string;
